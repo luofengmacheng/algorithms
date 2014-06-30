820ec8e507970302d252b837f7c26c631df252f4
luofengmacheng <674701120@qq.com>
2014-06-30 20:53:37 +0800

    2014/6/30

--- july_100/9.cpp
+++ july_100/9.cpp
@@ -2,6 +2,52 @@
  * author:	luo feng
  * date:	2014/6/30
  * source:	July的微软面试100题系列
- * title:	
+ * title:	9.判断整数序列是不是二叉查找树的后序遍历结果
  * language:	C++
+ * 本题主要是要明确二叉查找树的性质。判断一个序列是否是二叉查找树的后序遍历的方法是，如果一个序列是二叉查找树的后序遍历，那么最后一个元素就是这个树的根，
+ * 然后，从前往后遍历，找到第一个比根大的值a，那么根的左子树就是起始元素到a的前一个元素，从a开始到倒数第二个元素就是根的右子树，
+ * 而且左右子树必须都是二叉查找树。并且，还必须遍历从a开始到倒数第二个元素，检查它们是否比最后一个元素大。
  */
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+bool solve(vector<int>::iterator beg, vector<int>::iterator end)
+{
+	if(beg == end) {
+		return true;
+	}
+	int last = *(end-1);
+
+	vector<int>::iterator iter = beg;
+	while(iter != end-1 && *iter < last) {
+		++iter;
+	}
+	vector<int>::iterator iter2 = iter;
+	while(iter2 != end-1 && *iter2 >= last) {
+		++iter2;
+	}
+	if(iter2 != end-1) {
+		return false;
+	}
+	--end;
+
+	if(solve(beg, iter) && solve(iter, end)) {
+		return true;
+	}
+
+	return false;
+}
+
+int main(int argc, char const *argv[])
+{
+	int arr[] = {5, 7, 6, 9, 11, 10, 8};
+	vector<int> vec(arr, arr+sizeof(arr)/sizeof(arr[0]));
+	cout << boolalpha << solve(vec.begin(), vec.end()) << endl;
+
+	return 0;
+}
--- july_100/index.md
+++ july_100/index.md
@@ -2,4 +2,4 @@
 
 4 [在二叉查找树中找出和为某一值的所有路径](https://github.com/luofengmacheng/algorithms/blob/master/july_100/4.cpp)
 
-9 [判断整数序列是不是二叉查找树的后续便利结果](https://github.com/luofengmacheng/algorithms/blob/master/july_100/9.cpp)
+9 [判断整数序列是不是二叉查找树的后序遍历结果](https://github.com/luofengmacheng/algorithms/blob/master/july_100/9.cpp)
